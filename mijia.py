87cc57903e9c4293adfde5e7494e6b7a888f97d9
Eddy Almuiña <almuina.e@gmail.com>
2020-01-12 20:18:27 +0100

    Bugfix: try if exception occurs

--- mijia.py
+++ mijia.py
@@ -1,19 +1,23 @@
 import time
 
 from btlewrap import BluepyBackend
+from btlewrap.base import BluetoothBackendException
 from mitemp_bt.mitemp_bt_poller import MiTempBtPoller, MI_TEMPERATURE, MI_HUMIDITY
 
 
 def main():
     mac = "58:2D:34:33:D6:3C"
-    poller = MiTempBtPoller(mac, BluepyBackend, retries=100)
+    poller = MiTempBtPoller(mac, BluepyBackend)
 
     while True:
-        print(
-            time.time(),
-            poller.parameter_value(MI_TEMPERATURE, read_cached=False),
-            poller.parameter_value(MI_HUMIDITY, read_cached=False)
-        )
+        try:
+            print(
+                time.time(),
+                poller.parameter_value(MI_TEMPERATURE, read_cached=False),
+                poller.parameter_value(MI_HUMIDITY, read_cached=False)
+            )
+        except BluetoothBackendException:
+            continue
         time.sleep(60)
 
 
