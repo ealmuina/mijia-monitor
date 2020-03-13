7d3104d96f531c8995b71bfcdf416b6bc8dfd962
Eddy Almuiña <almuina.e@gmail.com>
2020-03-13 16:18:12 +0100

    Bugfix: run script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cd "$HOME/mijia-monitor/" || exit
 (
-  echo "python3 mijia.py";
-  echo "python3 bot.py 2> log.txt"
+  echo "venv/bin/python mijia.py";
+  echo "venv/bin/python bot.py 2> log.txt"
 ) | parallel
