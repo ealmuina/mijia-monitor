5732a921a08a0f3dbef592ec88b7b562535afbce
Eddy Almuiña <almuina.e@gmail.com>
2020-09-05 12:25:50 +0200

    Bugfix: run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,5 @@ cd "$HOME/mijia-monitor/" || exit
 (
   echo "venv/bin/python mijia.py > log_mijia.txt"
   echo "venv/bin/python bot.py 2> log_bot.txt"
-  echo "venv/bin/celery celery -A tasks worker --loglevel=info > log_celery.txt"
+  echo "venv/bin/celery -A tasks worker --loglevel=info > log_celery.txt"
 ) | parallel
