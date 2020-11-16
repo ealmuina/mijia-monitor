5112c888c71440b78b44fd1a243237ccecff0b42
Eddy Almuiña <almuina.e@gmail.com>
2020-11-16 01:00:41 +0100

    Bugfix: Run celery beat service

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,4 +3,5 @@ cd "$HOME/mijia-monitor/" || exit
   echo "venv/bin/python mijia.py > log_mijia.txt"
   echo "venv/bin/python bot.py 2> log_bot.txt"
   echo "venv/bin/celery -A tasks worker --loglevel=info > log_celery.txt"
+  echo "venv/bin/celery -A tasks beat --loglevel=info > log_celery_beat.txt"
 ) | parallel
