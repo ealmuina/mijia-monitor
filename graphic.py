1a36f146d0369f1d8d1dc3193c301f2053cfb57e
Eddy Almuiña <almuina.e@gmail.com>
2020-12-06 12:55:56 +0100

    Plot historical lines only once per graphics

--- graphic.py
+++ graphic.py
@@ -8,7 +8,7 @@ from model import Record
 cm = plt.cm.get_cmap('tab20c')
 
 
-def make_plt(start, location, temperature, humidity, single=True, i=0):
+def make_plt(start, location, temperature, humidity, single=True, i=0, historical_lines=False):
     records = Record.select().where(
         Record.date >= start,
         Record.location == location
@@ -22,25 +22,25 @@ def make_plt(start, location, temperature, humidity, single=True, i=0):
 
     if temperature:
         plt.plot(times, t, label='temperature' + ('' if single else f' {location.name}'), color=cm.colors[4 + i])
+        if historical_lines:
+            with open('utils/mean_temps.bin', 'rb') as file:
+                mean_temps = pickle.load(file)
+            tmax, tmin = [], []
+            for t in times:
+                date = '%02d-%02d' % (t.month, t.day)
+                tmax.append(mean_temps[date][0])
+                tmin.append(mean_temps[date][1])
+            plt.plot(times, tmax, label='max_temperature 1975-2019', color='Red')
+            plt.plot(times, tmin, label='min_temperature 1975-2019', color='Blue')
     if humidity:
         plt.plot(times, h, label='humidity' + ('' if single else f' {location.name}'), color=cm.colors[i])
-    elif location.outdoor:
-        with open('utils/mean_temps.bin', 'rb') as file:
-            mean_temps = pickle.load(file)
-        tmax, tmin = [], []
-        for t in times:
-            date = '%02d-%02d' % (t.month, t.day)
-            tmax.append(mean_temps[date][0])
-            tmin.append(mean_temps[date][1])
-        plt.plot(times, tmax, label='max_temperature 1975-2019', color='Red')
-        plt.plot(times, tmin, label='min_temperature 1975-2019', color='Blue')
 
 
 def single_plot(timespan, location, temperature=True, humidity=True):
     plt.figure(figsize=(11, 5.5))
 
     start = datetime.datetime.now() - timespan
-    make_plt(start, location, temperature, humidity)
+    make_plt(start, location, temperature, humidity, historical_lines=True)
 
     plt.title(location.name)
     plt.legend()
@@ -51,11 +51,16 @@ def single_plot(timespan, location, temperature=True, humidity=True):
 
 
 def multiple_plot(timespan, locations, temperature=True, humidity=True):
+    historical_lines = False
     plt.figure(figsize=(11, 5.5))
 
     start = datetime.datetime.now() - timespan
     for i, location in enumerate(locations):
-        make_plt(start, location, temperature, humidity, False, i)
+        if location.outdoor and not historical_lines:
+            make_plt(start, location, temperature, humidity, False, i, True)
+            historical_lines = True
+        else:
+            make_plt(start, location, temperature, humidity, False, i, False)
 
     plt.legend()
     plt.grid(b=True, which='major', color='#666666', linestyle='-')
