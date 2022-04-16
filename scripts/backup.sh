c42405dca71587b4ae1a1a595b8e6ff0b4953158
Eddy Almuiña <almuina.e@gmail.com>
2022-04-16 19:49:16 +0200

    Fixed directory name in backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # copy to local backups folder
-cd "$HOME/mijia/" || exit
+cd "$HOME/mijia-monitor/" || exit
 today=$(date '+%Y_%m_%d__%H_%M_%S')
-cp "mijia.db" "$HOME/backups/mijia/mijia_$today.db"
+cp "mijia.db" "$HOME/backups/mijia-monitor/mijia_$today.db"
 
 # backup to gdrive
 sleep 1
-gdrive push -quiet -destination "/" "$HOME/backups/mijia"
+gdrive push -quiet -destination "/" "$HOME/backups/mijia-monitor"
 
 # delete archives older than 7 days from disk
 sleep 1
-find "$HOME/backups/mijia/" -type f -mtime +7 -exec rm {} \;
+find "$HOME/backups/mijia-monitor/" -type f -mtime +7 -exec rm {} \;
