24431840d447451133ed589da8d9a34de6cea89b
Eddy Almuiña <almuina.e@gmail.com>
2023-01-27 13:59:15 +0100

    Enable CA certificate verification

--- mijia/utils.py
+++ mijia/utils.py
@@ -6,7 +6,6 @@ import paho.mqtt.client as mqtt
 def get_mqtt_client():
     client = mqtt.Client()
     client.tls_set(ca_certs=os.environ['CA_CERTS'])
-    client.tls_insecure_set(True)
     client.username_pw_set(
         username=os.environ['MQTT_USERNAME'],
         password=os.environ['MQTT_PASSWORD']
