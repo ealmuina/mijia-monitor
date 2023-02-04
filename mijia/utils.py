1b63c5872b9e229dc5c6ad1368afc042d7696188
Eddy Almuiña <almuina.e@gmail.com>
2023-02-04 13:59:28 +0100

    Connect clients using MQTT v5 protocol

--- mijia/utils.py
+++ mijia/utils.py
@@ -4,7 +4,9 @@ import paho.mqtt.client as mqtt
 
 
 def get_mqtt_client():
-    client = mqtt.Client()
+    client = mqtt.Client(
+        protocol=mqtt.MQTTv5
+    )
     client.tls_set(ca_certs=os.environ['CA_CERTS'])
     client.username_pw_set(
         username=os.environ['MQTT_USERNAME'],
